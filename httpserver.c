a0983c3bd3ac7cb4cd0de793e63095e26f794bd7
BlairNewmanJr <71617958+BlairNewmanJr@users.noreply.github.com>
2020-11-05 21:38:17 -0800

    Update httpserver.c

--- httpserver.c
+++ httpserver.c
@@ -129,11 +129,61 @@ void init_thread_pool(int num_threads, void (*request_handler)(int)) {
   /*
    * TODO: Part of your solution for Task 2 goes here!
    */
+  
+  threads_on_hold   = 0;
+	threads_keepalive = 1;
+	if (num_threads < 0){
+		num_threads = 0;
+	}
+	/* Make new thread pool */
+	thpool_* thpool_p;
+	thpool_p = (struct thpool_*)malloc(sizeof(struct thpool_));
+	if (thpool_p == NULL){
+		fprintf(stderr, "thpool_init(): Could not allocate memory for thread pool\n");
+		return NULL;
+	}
+	thpool_p->num_threads_alive   = 0;
+	thpool_p->num_threads_working = 0;
+	/* Initialise the job queue */
+	if (jobqueue_init(thpool_p) == -1){
+		fprintf(stderr, "thpool_init(): Could not allocate memory for job queue\n");
+		free(thpool_p);
+		return NULL;
+	}
+	/* Make threads in pool */
+	thpool_p->threads = (struct thread**)malloc(num_threads * sizeof(struct thread *));
+	if (thpool_p->threads == NULL){
+		fprintf(stderr, "thpool_init(): Could not allocate memory for threads\n");
+		jobqueue_destroy(thpool_p);
+		free(thpool_p->jobqueue_p);
+		free(thpool_p);
+		return NULL;
+	}
+
+	pthread_mutex_init(&(thpool_p->thcount_lock), NULL);
+	pthread_cond_init(&thpool_p->threads_all_idle, NULL);
+	
+
+	/* Thread init */
+	int n;
+	for (n=0; n<num_threads; n++){
+		thread_init(thpool_p, &thpool_p->threads[n], n);
+		if (THPOOL_DEBUG)
+			printf("THPOOL_DEBUG: Created thread %d in pool \n", n);
+	}
+	
+
+	/* Wait for threads to initialize */
+	while (thpool_p->num_threads_alive != num_threads) {}
+
+	return thpool_p;
+}
+  /*
   workers = (pthread_t *)calloc(num_threads, sizeof(pthread_t));
   for( int t =0;t<num_threads;t++){
     if(pthread_create(&workers[t],NULL,request_handler,0) != 0){
       //error do something
-      
+      */
     } 
   }
   
